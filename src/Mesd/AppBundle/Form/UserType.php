303ed25d85ee659b38ebb777cdc3812e4e0e5d1c
Oliver Forral <oforral@mesd.k12.or.us>
2014-11-04 09:57:34 -0800

    Added user crud

---
+++ src/Mesd/AppBundle/Form/UserType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Mesd\AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('roles')
+            ->add('groups')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Mesd\AppBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'mesd_appbundle_user';
+    }
+}
